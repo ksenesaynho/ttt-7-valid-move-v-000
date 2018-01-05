0ae8189cb47d14c815d3def25a8bdf92567022e8
Kristina Senesaynho <ksenesaynho@gmail.com>
2018-01-05 18:14:12 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
 def valid_move?(board, index)
   # if index.between?(1,9)
-  # if !position_taken?(board, index)
-  # true
-  # end
+  #   if !position_taken?(board, index)
+  #     true
+  #   end
   # end
   
   index.between?(0,8) ! position_taken?(board, index)
