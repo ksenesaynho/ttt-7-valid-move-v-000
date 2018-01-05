cd49f3d0234231bfb4aedb1bf333d580a49fc5fb
Kristina Senesaynho <ksenesaynho@gmail.com>
2018-01-05 18:01:19 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,17 +2,22 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board, index)
-# basic solution:
-# taken = nil
-# if (board[index] == “   “ || board[index] == nil)
-# taken = false
-# else
-# taken = true
-# end
-# taken
-
-# advanced solution w/ ternary operator
-(board[iundex] == “   “ || board[index] == “” || board[index] == nil) ?
-false : true
-end
+ def position_taken?(board, index) 
+   # basic solution: 
+ 
+ 
+   # taken = nil 
+   # if (board[index] ==  " " || board[index] == "" || board[index] == nil) 
+   #   taken = false 
+   # else 
+   #   taken = true 
+   # end 
+   # taken 
+ 
+ 
+   # advanced solution w/ ternary operator 
+   (board[index] ==  " " || board[index] == "" || board[index] == nil) ? 
+     false : true 
+ 
+ 
+ end 
